a8c1b6a533a796c85a09c093d8e5a1c6c8ec6e9a
Steph974-Git <10466@holbertonstudents.com>
2025-07-18 22:14:11 +0200

    Remove merge conflict markers from run.py

--- holberquest/run.py
+++ holberquest/run.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 from app.slack_bot.bot import slack_bot
 from flask import Flask
+from app import app
 
 app = Flask(__name__)
 app.register_blueprint(slack_bot)
-=======
-from app import app
+
 
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> origin/dev2
