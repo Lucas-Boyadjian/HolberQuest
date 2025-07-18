6fd90a115a9b0d4beeb8a25327c0d9d6e47c2d69
Steph974-Git <10466@holbertonstudents.com>
2025-07-18 19:05:40 +0200

    Remove merge conflict markers from config.py, profile.html, and helpers.py

--- holberquest/app/config.py
+++ holberquest/app/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Flask config (dev/prod)
 
 import os
@@ -9,5 +8,3 @@ load_dotenv()  # charge .env automatiquement
 class Config:
     SLACK_SIGNING_SECRET = os.getenv('SLACK_SIGNING_SECRET')
     SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
-=======
->>>>>>> origin/dev2
--- holberquest/app/utils/helpers.py
+++ holberquest/app/utils/helpers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Utility functions
 
 import os
@@ -50,5 +49,3 @@ def start_combat_for_user(user_id):
     send_message_to_user(user_id, "Une quête commence !", blocks=blocks)
 
     # À ce stade, il faut que tu crées un handler pour les réponses (interactions Slack) avec l’action "answer_qcm"
-=======
->>>>>>> origin/dev2
