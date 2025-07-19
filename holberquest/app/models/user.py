61bbf752dd6314b47ad3236260571c9b1210422b
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-07-19 08:16:24 +0200

    Remove duplicate import statement from user model

--- holberquest/app/models/user.py
+++ holberquest/app/models/user.py
@@ -2,8 +2,6 @@
 
 from app import db
 
-from app import db
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pseudo = db.Column(db.String(80), unique=True, nullable=False)
