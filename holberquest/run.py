ed0e433b6bf6d8b3408e966dbb8f73885bc58beb
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-07-19 08:19:52 +0200

    Add quest blueprint registration to the application

--- holberquest/run.py
+++ holberquest/run.py
@@ -4,6 +4,7 @@ from app import app
 from app.routes.main import main_bp
 from app.routes.combat import combat_bp
 from app.routes.auth import auth_bp
+from app.routes.quest import quest_bp
 from app.scheduler import scheduler  # Ajoute cette ligne pour démarrer le scheduler
 
 app.secret_key = "un_secret_pour_la_session"
@@ -12,6 +13,7 @@ app.register_blueprint(slack_bot)
 app.register_blueprint(main_bp)
 app.register_blueprint(combat_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(quest_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
